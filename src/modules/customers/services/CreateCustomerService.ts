fa761254b6fd1fc73580dd7c5c312fae5e77bb9a
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-10-21 01:10:33 -0300

    Criação de Customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,22 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
     // TODO
+    const customerExist = await this.customersRepository.findByEmail(email);
+
+    if (customerExist) {
+      throw new AppError('E-mail already in use');
+    }
+
+    const customer = this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
